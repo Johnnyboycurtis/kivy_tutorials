2d2f19892a0275d1d0cf1f3425f47bd949035060
Jonathan <johnnyboycurtis@gmail.com>
2019-07-21 15:16:16 -0700

    pong game

---
+++ demos/pong_app/pong_game.py
@@ -0,0 +1,21 @@
+"""
+https://kivy.org/doc/stable/tutorials/pong.html
+"""
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
+
+    def build(self):
+        return PongGame()
+
+
+if __name__ == "__main__":
+    PongApp().run()
+
