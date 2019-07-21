7f92c32bfb0505675d9d6d37c8da7ae2b2b116a2
Jonathan <johnnyboycurtis@gmail.com>
2019-07-21 15:28:18 -0700

    adding ball class

--- demos/pong_app/pong_game.py
+++ demos/pong_app/pong_game.py
@@ -4,6 +4,9 @@ https://kivy.org/doc/stable/tutorials/pong.html
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
 
 
 class PongGame(Widget):
@@ -14,6 +17,23 @@ class PongApp(App):
 
     def build(self):
         return PongGame()
+
+class PongBall(Widget):
+
+    # velocity of the ball on the x and y axes
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    # reference ReferenceListProperty so we can use ball
+    # a shorthand, just like e.g. w.pos for w.x and w.y
+
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    # `move` function will move the vall one step. 
+    # this will be called in equal intervals to animate the ball
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 if __name__ == "__main__":
